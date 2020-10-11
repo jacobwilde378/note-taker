8daf35ee6df29d85ffd14172b2923d5d3b407f06
Jacob.Wilde <jacob.wilde@utah.edu>
2020-10-11 17:07:27 -0600

    html routing

---
+++ server.js
@@ -0,0 +1,23 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const PORT = process.env.PORT || 80
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+// // app.use(express.json())
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use('/notes', htmlRoutes);
+app.use(express.static('public'));
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+})
